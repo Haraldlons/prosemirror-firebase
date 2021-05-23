53b05b5c3f65c0dc89499249c4cdb1d9979ba5a1
Harald Lønsethagen <haraldlons@gmail.com>
2021-05-23 11:47:21 +0200

    add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 import './AddTextEditor'
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 10px;
+`;
 
 function App() {
   return (
-  <>
-    <h3>Prosemirror and Firebase</h3>
-    <div id="editor" style={{marginBottom: "43px", backgroundColor:"lightgrey"}}></div>
-  </>
+    <Container>
+      <h3>Prosemirror and Firebase</h3>
+      <div id="editor" style={{marginBottom: "43px", backgroundColor:"lightgrey"}}></div>
+    </Container>
   );
 }
 
